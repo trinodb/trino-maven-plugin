71b188227528e7c0c6af00407a3e99028cda48bd
David Phillips <david@acz.org>
2016-08-20 10:02:55 -0700

    Rename package
    Signed-off-by: Jason van Zyl <jason@vanzyl.ca>

--- src/main/java/com/facebook/presto/maven/ServiceDescriptorGenerator.java
+++ src/main/java/com/facebook/presto/maven/ServiceDescriptorGenerator.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto;
+package com.facebook.presto.maven;
 
 import com.facebook.presto.spi.Plugin;
 import com.google.common.base.Charsets;
